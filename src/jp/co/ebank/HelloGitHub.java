62a819afaff559020d8a7f479850e60c3f8c96b3
Tamon <tamon.itaya@rakuten-bank.co.jp>
2015-11-02 15:23:10 +0900

    Add println

--- src/jp/co/ebank/HelloGitHub.java
+++ src/jp/co/ebank/HelloGitHub.java
@@ -3,7 +3,8 @@ package jp.co.ebank;
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello GitHub!");
 
 	}
 
